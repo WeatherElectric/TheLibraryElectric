6217d0993e6ccc69a777d0d2d074f9de4b70cb02
CarrionAndOn <maya@carrionandon.me>
2023-08-22 07:30:43 -0700

    i broke something

--- Main.cs
+++ Main.cs
@@ -10,14 +10,10 @@ namespace TheLibraryElectric
         internal const string Company = "Weather Electric";
         internal const string Version = "0.0.0";
         internal const string DownloadLink = "null";
-
-        public override void OnEarlyInitializeMelon()
+        public override void OnInitializeMelon()
         {
             FieldInjector.SerialisationHandler.Inject<KinematicRB>();
             FieldInjector.SerialisationHandler.Inject<DoNotFreeze>();
-        }
-        public override void OnInitializeMelon()
-        {
             FieldInjector.SerialisationHandler.Inject<CubeBreak>();
             FieldInjector.SerialisationHandler.Inject<FreezeRigidbodies>();
             FieldInjector.SerialisationHandler.Inject<DestroyOnCollision>();
