e02c5260c807c44ac51f5f8d574a2ea9580d5000
CarrionAndOn <maya@carrionandon.me>
2023-08-23 11:28:00 -0700

    Added signal recievers

---
+++ Scripts/Signals/SendSignal.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace TheLibraryElectric
+{
+#if UNITY_EDITOR
+[AddComponentMenu("The Library Electric/Send Signal")]
+#endif
+    public class SendSignal : MonoBehaviour
+    {
+        public string activationKey = "";
+        public void Broadcast()
+        {
+            RecieveSignal[] recievers = FindObjectsOfType<RecieveSignal>();
+            foreach (RecieveSignal reciever in recievers)
+            {
+                if (reciever != null && reciever.activationKey == activationKey)
+                {
+                    reciever.InvokeEvent();
+                }
+            }
+        }
+#if !UNITY_EDITOR
+        public SendSignal(IntPtr ptr) : base(ptr) { }
+#endif
+    }
+}
---
+++ UnityifiedScripts/RecieveSignal.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UltEvents;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace TheLibraryElectric
+{
+#if UNITY_EDITOR
+[AddComponentMenu("The Library Electric/Recieve Signal")]
+[RequireComponent(typeof(UltEventHolder))]
+#endif
+    public class RecieveSignal : MonoBehaviour
+    {
+        public string activationKey = "";
+        private UltEventHolder ultEvent;
+        private void Start()
+        {
+            ultEvent = GetComponent<UltEventHolder>();
+        }
+        public void InvokeEvent()
+        {
+            if (ultEvent != null)
+            {
+                ultEvent.Invoke();
+            }
+            else
+            {
+                Debug.LogWarning("No ultevent present!");
+            }
+        }
+    }
+}
