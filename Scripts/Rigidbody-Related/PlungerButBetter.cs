ad7f2ebc756cedc6f5b4c0b1278e6a9d359737de
MrBugo <bugobug20@gmail.com>
2023-08-24 12:53:39 +0300

    Create PlungerButBetter.cs

---
+++ Scripts/Rigidbody-Related/PlungerButBetter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TheLibraryElectric.Scripts.Rigidbody_Related
+{
+    public class PlungerButBetter : MonoBehaviour
+    {
+        [Flags]
+        public enum localAxisLimit
+        {
+            X = 1 << 0, // 1
+            Y = 1 << 1, // 2
+            Z = 1 << 2  // 4
+        }
+
+        public localAxisLimit selectedLimits;
+
+    }
+
+}
