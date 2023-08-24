f3fdabcf74d69a8a473eb2404042aeade7f79157
CarrionAndOn <maya@carrionandon.me>
2023-08-23 20:53:37 -0700

    Changed signals back to strings

--- DummyScripts/RecieveSignal.cs
+++ DummyScripts/RecieveSignal.cs
@@ -12,37 +12,7 @@ namespace TheLibraryElectric
 #endif
     public class RecieveSignal : MonoBehaviour
     {
-		public enum ActivationKey
-        {
-            CODE_A =1,
-            CODE_B =2,
-            CODE_C =3,
-            CODE_D =4,
-            CODE_E =5,
-            CODE_F =6,
-            CODE_G =7,
-            CODE_H =8,
-            CODE_I =9,
-            CODE_J =10,
-            CODE_K =11,
-            CODE_L =12,
-            CODE_M =13,
-            CODE_N =14,
-            CODE_O =15,
-            CODE_P =16,
-            CODE_Q =17,
-            CODE_R =18,
-            CODE_S =19,  
-            CODE_T =20,
-            CODE_U =21,
-            CODE_V =22,
-            CODE_W =23,
-            CODE_X =24,
-            CODE_Y =25,
-            CODE_Z =26,
-        }
-        public ActivationKey activationKey;
-
+		public string activationKey;
         private UltEventHolder ultEvent;
         private void Start()
         {
