7925cb3a09c7ea70bc22cdc3708028f83c5a025b
CarrionAndOn <maya@carrionandon.me>
2023-08-24 18:42:21 -0700

    Added updateTMP

---
+++ Scripts/Misc/UpdateTMP.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using TMPro;
+using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace TheLibraryElectric
+{
+#if UNITY_EDITOR
+    [AddComponentMenu("The Library Electric/Misc/Update TextMeshPro")}
+#endif
+    public class UpdateTMP : MonoBehaviour
+    {
+        public TextMeshPro textMeshPro;
+        public float startingValue = 1.0f;
+        public float incrementAmount = 0.5f;
+
+        private float currentValue;
+
+        private void Start()
+        {
+            currentValue = startingValue;
+            UpdateTextValue();
+        }
+
+        private void UpdateTextValue()
+        {
+            textMeshPro.text = currentValue.ToString("F1"); // Displaying with one decimal place
+        }
+
+        public void IncreaseValue()
+        {
+            currentValue += incrementAmount;
+            UpdateTextValue();
+        }
+        public void DecreaseValue()
+        {
+            currentValue -= incrementAmount;
+            if (currentValue < 0.0f)
+            {
+                currentValue = 0.0f;
+            }
+            UpdateTextValue();
+        }
+#if !UNITY_EDITOR
+        public UpdateTMP(IntPtr ptr) : base(ptr) { }
+#endif
+    }
+}
