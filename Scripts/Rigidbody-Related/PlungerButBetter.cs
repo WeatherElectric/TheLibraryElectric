8b307b3da7f2c0b4f38e9e51c134c21692683a34
CarrionAndOn <maya@carrionandon.me>
2023-08-28 13:39:45 -0700

    Fixed a namespace

--- Scripts/Rigidbody-Related/PlungerButBetter.cs
+++ Scripts/Rigidbody-Related/PlungerButBetter.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace TheLibraryElectric.Scripts.Rigidbody_Related
+namespace TheLibraryElectric
 {
     public class PlungerButBetter : MonoBehaviour
     {
