6be5301c14612e9e166e2536d1490475aa04ab39
CarrionAndOn <maya@carrionandon.me>
2023-09-09 02:57:53 -0700

    Whoops lol

--- DummyScripts/DoNotDestroy.cs
+++ DummyScripts/DoNotDestroy.cs
@@ -16,8 +16,5 @@ namespace TheLibraryElectric.Markers
         [TextArea(3, 10)] // Allows for multiline input in the Inspector
         private const string usageNote = "This component must be on the root of your object.";
 #endif
-#if !UNITY_EDITOR
-        public DoNotDestroy(IntPtr ptr) : base(ptr) { }
-#endif
     }
 }
