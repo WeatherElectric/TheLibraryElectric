5d0a7c92debef7217a554e90f14c5d9b622fdca4
CarrionAndOn <maya@carrionandon.me>
2023-09-12 16:58:59 -0700

    Organization, SpawnCrate helper, SpawnCrateOnTriggerEnter.cs

---
+++ InternalHelpers/SpawnCrate.cs
@@ -0,0 +1,27 @@
+using System;
+using BoneLib.Nullables;
+using SLZ.Marrow.Data;
+using SLZ.Marrow.Pool;
+using UnityEngine;
+using SLZ.Marrow.Warehouse;
+
+namespace TheLibraryElectric.InternalHelpers
+{
+    public class SpawnCrate
+    {
+        public static void Spawn(string barcode, Vector3 position, Quaternion rotation, bool ignorePolicy, Action<GameObject> callback)
+        {
+            SpawnableCrateReference reference = new SpawnableCrateReference(barcode);
+            Spawnable spawnable = new Spawnable()
+            {
+                crateRef = reference
+            };
+            AssetSpawner.Register(spawnable);
+            Action<GameObject> spawnAction = go =>
+            {
+                callback(go);
+            };
+            AssetSpawner.Spawn(spawnable, position, rotation, new BoxedNullable<Vector3>(null), ignorePolicy, new BoxedNullable<int>(null), spawnAction);
+        }
+    }
+}
