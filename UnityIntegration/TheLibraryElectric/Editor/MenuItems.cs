a19271e9c3f2ec33ad33407f20add858eaa620e6
CarrionAndOn <maya@carrionandon.me>
2023-09-15 17:17:34 -0700

    Still dumb

--- UnityIntegration/TheLibraryElectric/Editor/MenuItems.cs
+++ UnityIntegration/TheLibraryElectric/Editor/MenuItems.cs
@@ -16,6 +16,17 @@ public class MenuItems : Editor
 		Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
 		Selection.activeObject = go;
 	}
+	[MenuItem("GameObject/The Library Electric/Water Zone", false, 10)]
+	static void CreateWaterZone(MenuCommand menuCommand)
+	{
+		GameObject go = new GameObject("Water Zone");
+		GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
+		BoxCollider bc = go.AddComponent<BoxCollider>();
+		bc.isTrigger = true;
+		go.AddComponent<WaterZone>();
+		Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+		Selection.activeObject = go;
+	}
 	[MenuItem("GameObject/The Library Electric/Ragdoll Zone", false, 10)]
 	static void CreateRagdollZone(MenuCommand menuCommand)
 	{
