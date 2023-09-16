422823ebc4146a953d5c5a3b784b4be079416bc0
MrBugo <bugobug20@gmail.com>
2023-09-16 15:53:06 +0300

    Man I'm dumb I should not be trusted with this

--- Monobehaviours/Misc/SpawnOnTriggerEnter.cs
+++ Monobehaviours/Misc/SpawnOnTriggerEnter.cs
@@ -15,10 +15,15 @@ namespace TheLibraryElectric.Misc
 
         public GameObject objectToSpawn;
         public bool divideVelocity;
-        public float divideByX = 0.1f;
-        public float divideByY = 0.1f;
-        public float divideByZ = 0.1f;
+        public float divideByX = 3f;
+        public float divideByY = 3f;
+        public float divideByZ = 3f;
         public Vector3 minimumScale = new Vector3(1, 1, 1);
+        public Vector3 maximumScale = new Vector3(200, 200, 200);
+        public bool uniformScale = true;
+        public float uniformScaleMinimum = 1f;
+        public float uniformScaleMaximum = 200f;
+        public float uniformScaleDivision = 3f;
         public bool ignoreSpawnedObjects = false;
         public bool fixedRotation = true;
         public Quaternion rotation = new Quaternion(0, 0, 1, 0);
@@ -33,7 +38,7 @@ namespace TheLibraryElectric.Misc
             {
                 return;
             }
-            if (spawnedObjects.Contains(other.transform.root.gameObject))
+            if (spawnedObjects.Contains(other.transform.root.gameObject) && ignoreSpawnedObjects)
             {
                 return;
             }
@@ -50,12 +55,21 @@ namespace TheLibraryElectric.Misc
                 spawnedObject = Instantiate(objectToSpawn, other.transform.position, other.transform.rotation);
 
             }
+            if (!uniformScale)
+            {
+                float clampedX = Mathf.Clamp(other.attachedRigidbody.velocity.x / divideByX, minimumScale.x, maximumScale.x);
+                float clampedY = Mathf.Clamp(other.attachedRigidbody.velocity.y / divideByY, minimumScale.y, maximumScale.y);
+                float clampedZ = Mathf.Clamp(other.attachedRigidbody.velocity.z / divideByZ, minimumScale.z, maximumScale.z);
+                rbVelocity = new Vector3(clampedX, clampedY, clampedZ);
+                spawnedObject.transform.localScale = Vector3.Scale(rbVelocity, spawnedObject.transform.lossyScale);
+            }
+            else
+            {
+                rbVelocity = new Vector3(other.attachedRigidbody.velocity.x, other.attachedRigidbody.velocity.y, other.attachedRigidbody.velocity.z);
+                float uniformScale = Mathf.Clamp(rbVelocity.sqrMagnitude / uniformScaleDivision, uniformScaleMinimum, uniformScaleMinimum);
+                Vector3 newScale = new Vector3(uniformScale, uniformScale, uniformScale);
+            }
 
-            float clampedX = Mathf.Clamp(other.attachedRigidbody.velocity.x / divideByX, minimumScale.x, 200f);
-            float clampedY = Mathf.Clamp(other.attachedRigidbody.velocity.y / divideByY, minimumScale.y, 200f);
-            float clampedZ = Mathf.Clamp(other.attachedRigidbody.velocity.z / divideByZ, minimumScale.z, 200f);
-            rbVelocity = new Vector3(clampedX, clampedY, clampedZ);
-            spawnedObject.transform.localScale = Vector3.Scale(rbVelocity, spawnedObject.transform.lossyScale);
         }
 #if !UNITY_EDITOR
         public SpawnOnTriggerEnter(IntPtr ptr) : base(ptr) { }
