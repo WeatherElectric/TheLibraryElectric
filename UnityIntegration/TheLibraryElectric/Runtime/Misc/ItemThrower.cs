ad71f0033fb8607e6329710bb87d91765babdc09
MrBugo <bugobug20@gmail.com>
2023-09-16 16:33:33 +0300

    Added Item Thrower

---
+++ UnityIntegration/TheLibraryElectric/Runtime/Misc/ItemThrower.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using SLZ.Marrow.Data;
+
+
+namespace TheLibraryElectric.Misc
+{
+#if UNITY_EDITOR
+    [AddComponentMenu("The Library Electric/Misc/Item Thrower")]
+#endif
+    public class ItemThrower : MonoBehaviour
+    {
+        public Spawnable spawnable;
+        public Vector3 force;
+        void Throw()
+        {
+
+        }
+    }
+}
