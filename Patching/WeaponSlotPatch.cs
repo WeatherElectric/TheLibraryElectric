c069fd38b6921957f0a8ea05898326dcadaa1284
CarrionAndOn <maya@carrionandon.me>
2023-09-18 19:39:20 -0700

    whoopsi

--- Patching/WeaponSlotPatch.cs
+++ Patching/WeaponSlotPatch.cs
@@ -8,18 +8,18 @@ namespace TheLibraryElectric.Patching
 {
     public static class WeaponSlotPatch
     {
-        [HarmonyPatch(typeof(InteractableHost), "onSlotInsert")]
+        [HarmonyPatch(typeof(InteractableHost), "InsertInSlot")]
         public class InteractableHostPatch
         {
             [HarmonyPrefix]
             public static void Postfix(GameObject __instance)
             {
-                Component buoyancyManager = __instance.GetComponentInParent<RbBuoyancyManager>();
+                Component buoyancyManager = __instance.GetComponent<RbBuoyancyManager>();
                 if (buoyancyManager != null)
                 {
                     Object.Destroy(buoyancyManager);
                 }
-                Component gravityManager = __instance.GetComponentInParent<RBGravityManager>();
+                Component gravityManager = __instance.GetComponent<RBGravityManager>();
                 if (gravityManager != null)
                 {
                     Object.Destroy(gravityManager);
