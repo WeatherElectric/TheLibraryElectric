cf1a5bbbd9f80ad49b6f75b8ab4159e5b43d2703
CarrionAndOn <maya@carrionandon.me>
2023-09-20 11:09:42 -0700

    remove drown sound, didnt work

--- Monobehaviours/Water/DrowningManager.cs
+++ Monobehaviours/Water/DrowningManager.cs
@@ -17,17 +17,7 @@ namespace TheLibraryElectric.Water
         public float timeUntilDrowning = 20f;
         public float damageInterval = 1f;
         public float damagePercentage = 0.10f;
-        public bool enableSoundQueue = false;
-#if UNITY_EDITOR
-        [Tooltip("The sound that plays when the player is halfway to drowning.")]
-#endif
-        public AudioSource halfWarningSound;
-#if UNITY_EDITOR
-        [Tooltip("The sound that plays when the player is a quarter of the way to drowning.")]
-#endif
-        public AudioSource quarterWarningSound;
         private RigManager _rigManager;
-        private Transform _head;
         private bool _isDrowning = false;
         private float _timeInsideWater = 0.0f;
         private float _methodCallTimer = 0.0f;
@@ -37,7 +27,6 @@ namespace TheLibraryElectric.Water
             _rigManager = other.GetComponentInParent<RigManager>();
             if (_rigManager != null)
             {
-                _head = _rigManager.openControllerRig.m_head;
                 _isDrowning = true;
             }
         }
@@ -51,16 +40,6 @@ namespace TheLibraryElectric.Water
             if (_isDrowning)
             {
                 _timeInsideWater += Time.deltaTime;
-                if (enableSoundQueue && _timeInsideWater >= timeUntilDrowning / 2)
-                {
-                    halfWarningSound.transform.SetParent(_head);
-                    halfWarningSound.Play();
-                }
-                if (enableSoundQueue && _timeInsideWater >= timeUntilDrowning / 4)
-                {
-                    quarterWarningSound.transform.SetParent(_head);
-                    quarterWarningSound.Play();
-                }
                 if (_timeInsideWater >= timeUntilDrowning)
                 {
                     _methodCallTimer += Time.deltaTime;
