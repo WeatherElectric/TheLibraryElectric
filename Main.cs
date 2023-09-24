a29d4bbeb7d8d43ad5dfe77498888688e4f787d9
CarrionAndOn <maya@carrionandon.me>
2023-09-24 13:02:50 -0700

    Fixed a bug in rbuoyancymanager that's caused by using a gravity chamber near a waterzone

--- Main.cs
+++ Main.cs
@@ -10,7 +10,7 @@ namespace TheLibraryElectric
         internal const string Description = "See how IL2CPP breaks the mod.";
         internal const string Author = "CarrionAndOn + BugoBug";
         internal const string Company = "Weather Electric";
-        internal const string Version = "1.3.0";
+        internal const string Version = "1.3.1";
         internal const string DownloadLink = "https://bonelab.thunderstore.io/package/CarrionAndOn/TheLibraryElectric/";
         public override void OnInitializeMelon()
         {
