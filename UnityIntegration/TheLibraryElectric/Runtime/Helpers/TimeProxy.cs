6160e0e9284610905e480860e8fd9ee26b5d2ee1
MrBugo <bugobug20@gmail.com>
2023-10-11 16:21:07 +0300

    Added TimeProxy

---
+++ UnityIntegration/TheLibraryElectric/Runtime/Helpers/TimeProxy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace TheLibraryElectric.InternalHelpers
+{
+    public static class TimeProxy
+    {
+        public static int GetMinute()
+        {
+            return System.DateTime.Now.Minute;
+        }
+        public static int GetHour()
+        {
+            return System.DateTime.Now.Hour;
+        }
+        public static bool IfAfterOrNow(float hour, float minute)
+        {
+            if(hour >= GetHour() && minute >= minute)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IfBeforeOrNow(float hour, float minute)
+        {
+            if (hour <= GetHour() && minute <= minute)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
