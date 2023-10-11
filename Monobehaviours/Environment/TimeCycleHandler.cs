6aafe033b05af9954c15c5a16cf7fb5bdbbbd6cb
CarrionAndOn <maya@carrionandon.me>
2023-10-11 13:51:12 -0700

    Made day night cycle script adjust intensity of reflection probes too

--- Monobehaviours/Environment/TimeCycleHandler.cs
+++ Monobehaviours/Environment/TimeCycleHandler.cs
@@ -8,6 +8,11 @@ namespace TheLibraryElectric.Environment
         public Light sun;
         public float yAxis;
         public float zAxis;
+        public ReflectionProbe[] reflectionProbes;
+        public float nightIntensity = 0.4f;
+        public float dayIntensity = 1.0f;
+        public float nightStartHour = 19.0f;
+        public float nightEndHour = 6.0f;
 
         private void Update()
         {
@@ -18,6 +23,23 @@ namespace TheLibraryElectric.Environment
             var totalSeconds = hours * 3600 + minutes * 60 + seconds;
             var angle = 90 - (totalSeconds / 86400) * 360;
             sun.transform.rotation = Quaternion.Euler(-angle, yAxis, zAxis);
+            var intensity = CalculateIntensity(hours, minutes);
+            foreach (var reflectionProbe in reflectionProbes)
+            {
+                reflectionProbe.intensity = intensity;
+            }
+        }
+        
+        private float CalculateIntensity(float hours, float minutes)
+        {
+            if (hours >= nightStartHour || hours < nightEndHour)
+            {
+                return nightIntensity;
+            }
+            else
+            {
+                return dayIntensity;
+            }
         }
 #if !UNITY_EDITOR
         public TimeCycleHandler(IntPtr ptr) : base(ptr) { }
--- UnityIntegration/TheLibraryElectric/Runtime/Environment/TimeCycleHandler.cs
+++ UnityIntegration/TheLibraryElectric/Runtime/Environment/TimeCycleHandler.cs
@@ -9,11 +9,23 @@ namespace TheLibraryElectric.Environment
 		[Header("Light")]
 		[Tooltip("The directional light that will be rotated.")]
         public Light sun;
-		[Header("Extra Rotation")]
+		[Header("Light Settings")]
 		[Tooltip("What should the light's rotation on the Y axis be?")]
         public float yAxis;
 		[Tooltip("What should the light's rotation on the Z axis be?")]
         public float zAxis;
+        [Tooltip("The reflection probe(s) to change the intensity of.")]
+        public ReflectionProbe[] reflectionProbes;
+        [Header("Reflection Probe Settings")]
+        [Tooltip("What should the reflection probe's intensity be at night?")]
+        public float nightIntensity = 0.4f;
+        [Tooltip("What should the reflection probe's intensity be at day?")]
+        public float dayIntensity = 1.0f;
+        [Header("Time Settings")] 
+        [Tooltip("What hour should the night start? 24 hour format")]
+        public float nightStartHour = 19.0f;
+	    [Tooltip("What hour should the night end? 24 hour format")]
+        public float nightEndHour = 6.0f;
 
         private void Update()
         {
@@ -24,6 +36,23 @@ namespace TheLibraryElectric.Environment
             var totalSeconds = hours * 3600 + minutes * 60 + seconds;
             var angle = 90 - (totalSeconds / 86400) * 360;
             sun.transform.rotation = Quaternion.Euler(-angle, yAxis, zAxis);
+            var intensity = CalculateIntensity(hours, minutes);
+            foreach (var reflectionProbe in reflectionProbes)
+            {
+	            reflectionProbe.intensity = intensity;
+            }
+        }
+        
+        private float CalculateIntensity(float hours, float minutes)
+        {
+	        if (hours >= nightStartHour || hours < nightEndHour)
+	        {
+		        return nightIntensity;
+	        }
+	        else
+	        {
+		        return dayIntensity;
+	        }
         }
     }
 }
